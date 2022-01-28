1330d93706ec13a4f419b245603195152f1fadbd
XLab59 <rana.zain909@gmail.com>
2022-01-28 19:57:42 +0500

    fixed a bug
    bug of displaying recipes on main page even it was deleted

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
 @Component({
@@ -6,13 +6,19 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit  {
   recipes: Recipe[];
 
 constructor(private recipesService: RecipesService) {}
 ngOnInit(): void {
-  this.recipes = this.recipesService.getAllRecipes();
+
 }
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+
+
 
 }
 
